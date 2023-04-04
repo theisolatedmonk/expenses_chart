e790516d067453658279f126967bf02e18776124
theisolatedmonk <96rahulseth@gmail.com>
2023-04-04 13:56:19 +0530

    graph and tooltip done

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 const balamt = document.getElementById('balamt');
 const totalamt = document.getElementById('totalamt');
 const percent = document.getElementById('percent');
-const monAmt = document.getElementById('mon_amt');
-const tueAmt = document.getElementById('tue_amt');
-const wedAmt = document.getElementById('wed_amt');
-const thuAmt = document.getElementById('thu_amt');
-const friAmt = document.getElementById('fri_amt');
-const satAmt = document.getElementById('sat_amt');
-const sunAmt = document.getElementById('sun_amt');
+
+const mondayDiv = document.getElementById('mon1');
+const tuedayDiv = document.getElementById('tue1');
+const wednesdayDiv = document.getElementById('wed1');
+const thursdayDiv = document.getElementById('thu1');
+const fridayDiv = document.getElementById('fri1');
+const saturdayDiv = document.getElementById('sat1');
+const sundayDiv = document.getElementById('sun1');
+
+const mondayAmt = document.getElementById('mon_amt');
+const tuesdayAmt = document.getElementById('tue_amt');
+const wednesdayAmt = document.getElementById('wed_amt')
+const thursdayAmt = document.getElementById('thu_amt');
+const fridayAmt = document.getElementById('fri_amt');
+const saturdayAmt = document.getElementById('sat_amt');
+const sundayAmt = document.getElementById('sun_amt');
+
+
+const days = [mondayDiv, tuedayDiv, wednesdayDiv, thursdayDiv, fridayDiv, saturdayDiv, sundayDiv]
+const daysAmt = [mondayAmt, tuesdayAmt, wednesdayAmt, thursdayAmt, fridayAmt, saturdayAmt, sundayAmt]
+
+fetch('\data.json')
+    .then(response => response.json())
+    .then(data => {
+        let sum = 0;
+        for (let i = 0; i < data.length; i++) {
+            if (typeof data[i].amount === 'number') {
+                sum += data[i].amount;
+            }
+        }
+        totalamt.innerHTML = `$ ${sum}`;
+        // console.log(sum);
+
+
+        for (let i = 0; i < days.length; i++) {
+            days[i].style.height = `${data[i].amount}px`
+        }
+
+        for (let i = 0; i < daysAmt.length; i++) {
+            daysAmt[i].innerHTML = `${data[i].amount}`
+        }
+
+        // console.log(mondayDiv.)
+
+        // days[0].style.height = `${data[0].amount}px`
+
+
+
+    })
+    .catch(error => console.error(error));
